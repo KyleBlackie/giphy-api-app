f349be9eaf15ffca8968decbbbac4acfd66c50de
Kyle Blackie <kyle.blackie@mail.utoronto.ca>
2020-09-30 09:54:11 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const input = document.getElementById('search-term');
 
 function getGif(searchTerm) {
   // fetch data from the giphy api using the searchTerm argument or the text input value
+  // NOTE: Unfortunately, since this project is done without a backend there is no way to properly protect the api key
+  //       If this were a full stack app I would have kept the api key secure / hidden 
   fetch(`https://api.giphy.com/v1/gifs/translate?api_key=Z94VXVe61QA2VBdPmfjghtxiSnJZPnHF&s=${typeof searchTerm === 'undefined' ? input.value : searchTerm}`, {mode: 'cors'})
   .then(response => response.json())
   .then(response => img.src = response.data.images.original.url)
